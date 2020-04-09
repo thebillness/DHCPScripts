6b7851621a9efbfc1717be8c03d7a5cc92fc189f
Bill Wall <thebillness@users.noreply.github.com>
2020-04-09 10:07:10 -0500

    Added Active Leases Only as an optional parameter

--- Export-LeaseData.ps1
+++ Export-LeaseData.ps1
@@ -2,12 +2,13 @@ param (
     [string[]] $DHCPServer,
     [string[]] $ScopeID,
     [string] $OutputPath,
-    [bool] $NoClobber = $false
+    [bool] $NoClobber = $false,
+    [bool] $OnlyActiveLeases = $false
 )
 
 $myScopes = @()
 ForEach ($server in $DHCPServer) {
-    $myScopes += Get-DhcpServerv4Scope -ComputerName $server -ScopeId $ScopeID | Select-Object Name, ScopeId
+    $myScopes += Get-DhcpServerv4Scope -ComputerName $server | Where-Object {$_.ScopeId -like $ScopeID} | Select-Object Name, ScopeId
 }
 $myScopes = $myScopes | Select-Object * -Unique
 
@@ -17,5 +18,6 @@ ForEach ($scope in $myScopes) {
         ForEach ($myServer in $DHCPServer) {
             $myLeases += Get-DhcpServerv4Lease -ComputerName $myServer -ScopeId $scope.ScopeID | Select-Object IPAddress,AddressState,ClientID,HostName,Description
         }
-    $myLeases| Select-Object * -Unique | Export-Csv -NoTypeInformation -Path (Join-Path $outputPath -ChildPath  "Leases_$($scope.ScopeID).csv") -NoClobber:$NoClobber
+    If ($OnlyActiveLeases) {$myLeases = $myLeases | Where-Object {$_.AddressState -eq "Active"}}
+    $myLeases | Select-Object * -Unique | Export-Csv -NoTypeInformation -Path (Join-Path $outputPath -ChildPath  "Leases_$($scope.ScopeID).csv") -NoClobber:$NoClobber
 }
